4aa9a71932db2268156e2fc91670341ffd665ff4
talithasahdaaa <sahda5824@gmail.com>
2025-07-06 18:14:12 +0700

    halo

--- koneksimysql.php
+++ koneksimysql.php
@@ -1,16 +1,16 @@
 <?php
 
 // menggunakan xampp
-// define('host', 'localhost');
-// define('user', 'root');
-// define('password', '');
-// define('database', 'gramedia');
+define('host', 'localhost');
+define('user', 'root');
+define('password', '');
+define('database', 'androidtalita');
 
 // menggunakan hosting
-define('host', 'localhost');
-define('user', 'androidtalita');
-define('password', 'Talita123');
-define('database', 'androidtalita');
+// define('host', 'localhost');
+// define('user', 'androidtalita');
+// define('password', 'Talita123');
+// define('database', 'androidtalita');
 
 $conn = mysqli_connect(host, user, password, database);
 if (!$conn) {
